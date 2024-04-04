972e9f88fa8eaecd586dcae84e3e5f4029557fd5
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-04-04 15:42:19 +0000

    code

--- Binary_Search_2D.java
+++ Binary_Search_2D.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class Binary_Search_2D {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        int fibo = (int)Math.pow((1 + Math.sqrt(5)) / 2, n) - (int)Math.pow((1 - Math.sqrt(5)) / 2, n); 
+        System.out.println(fibo / (int)Math.sqrt(5));
     }
 }
