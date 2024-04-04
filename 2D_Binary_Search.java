f19a96cf9d48ca3235cd8ec0618c9c19b76ef464
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-04-04 15:17:44 +0000

    edit

---
+++ 2D_Binary_Search.java
@@ -0,0 +1,5 @@
+public class 2DBinary_Search {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Binary_Search_2D.java
@@ -0,0 +1,5 @@
+public class Binary_Search_2D {
+    public static void main(String[] args) {
+        
+    }
+}
