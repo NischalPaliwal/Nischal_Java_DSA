c23dd8d5885e928e845d3ab71c4c27cb7dda6ed2
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-02-14 07:33:10 +0000

    code added

--- recursion/sub-sequences/skipChar.java
+++ recursion/sub-sequences/skipChar.java
@@ -3,9 +3,47 @@ public class skipChar {
 
     }
 
-    public static void skip_char(String p, String up) {
-        if() {
+    public static void skip_char_1(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
 
+        char ch = up.charAt(0);
+
+        if (ch == 'a') {
+            skip_char_1(p, up.substring(1));
+        } else {
+            skip_char_1(p + ch, up.substring(1));
         }
     }
+
+    public static String skip_char_2(String p, String up) {
+        if (up.isEmpty()) {
+            return "";
+        }
+
+        char ch = up.charAt(0);
+
+        if (ch == 'a') {
+            return skip_char_2(p, up.substring(1));
+        } else {
+            return skip_char_2(p + ch, up.substring(1));
+        }
+    }
+
+    public static void skipString(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+
+        if (up.startsWith("apple")) {
+            skipString(p, up.substring(5));
+        } else {
+            skipString(p + up.charAt(0), up.substring(1));
+        }
+    }
+
+    
 }
