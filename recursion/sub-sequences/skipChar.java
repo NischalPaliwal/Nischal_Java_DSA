86c2e5775c202e1a89502807e05c0594543632c8
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-02-14 08:13:54 +0000

    code added

--- recursion/sub-sequences/skipChar.java
+++ recursion/sub-sequences/skipChar.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class skipChar {
     public static void main(String[] args) {
 
@@ -18,7 +20,7 @@ public class skipChar {
         }
     }
 
-    public static String skip_char_2(String p, String up) {
+    public static String skip_char_2(String up) {
         if (up.isEmpty()) {
             return "";
         }
@@ -26,9 +28,9 @@ public class skipChar {
         char ch = up.charAt(0);
 
         if (ch == 'a') {
-            return skip_char_2(p, up.substring(1));
+            return skip_char_2(up.substring(1));
         } else {
-            return skip_char_2(p + ch, up.substring(1));
+            return ch + skip_char_2(up.substring(1));
         }
     }
 
@@ -45,5 +47,47 @@ public class skipChar {
         }
     }
 
-    
+    public static void subSeq_1(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+
+        subSeq_1(p + ch, up.substring(1));
+        subSeq_1(p, up.substring(1));
+    }
+
+    public static ArrayList<String> subSeq_2(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+        
+        ArrayList<String> left = subSeq_2(p + ch, up.substring(1));
+        ArrayList<String> right = subSeq_2(p, up.substring(1));
+
+        left.addAll(right);
+        return left;
+    }
+
+    public static ArrayList<ArrayList<Integer>> subSet(int[] arr) {
+        ArrayList<ArrayList<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        for (int num : arr) {
+            int n = outer.size();
+            for (int i = 0; i < n; i++) {
+                ArrayList<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+
+        return outer;
+    }
 }
