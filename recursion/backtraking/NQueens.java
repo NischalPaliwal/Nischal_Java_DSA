1de9c53d1f6757bd6f2e1a8ce322a42ef20df7f0
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-02-21 10:22:48 +0000

    code added

--- recursion/backtraking/NQueens.java
+++ recursion/backtraking/NQueens.java
@@ -2,7 +2,7 @@ package recursion.backtraking;
 
 public class NQueens {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 4;
         boolean[][] board = new boolean[n][n];
         System.out.println(queens(board, 0));
     }
