e1ac8fd8d3386bef281358c845313343dbb38728
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-03-20 18:43:39 +0000

    code added

--- oops/oop1.java
+++ oops/oop1.java
@@ -5,6 +5,27 @@ public class oop1 {
         Student[] students = new Student[5];
         students[0] = new Student(101, "Nischal Paliwal", "3-06-2004", 20, 11, 'A');
         // System.out.println(students[0]);
+        Integer num = 45;
+        System.out.println(num.hashCode());
+
+        Integer a = 5;
+        Integer b = 6;
+
+        swap(a, b);
+        System.out.println(a + "  " + b); // swapping didn't happened
+
+        final int phoneNo = 701;
+        // phoneNo = 343; value of variable cannot be changed
+        // final variables must be initialized with a value while declaration
+        final int[] arr = {1, 2, 3, 4, 5, 6};
+        arr[2] = 1;
+        
+    }
+
+    static void swap(Integer a, Integer b) {
+        Integer temp = a;
+        a = b;
+        b = temp;
     }
 }
 
