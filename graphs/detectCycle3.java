bfc26327dc10cd719b546ad11109c1b12468ce03
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-03-27 17:18:04 +0000

    code added

---
+++ graphs/detectCycle3.java
@@ -0,0 +1,39 @@
+package graphs;
+
+import java.util.ArrayList;
+
+public class detectCycle3 {
+    public static void main(String[] args) {
+        
+    }
+
+    private boolean dfs(int node, ArrayList<ArrayList<Integer>> adj, boolean[] vis, boolean[] pathVis) {
+        vis[node] = true;
+        pathVis[node] = true;
+
+        for (int neighbor : adj.get(node)) {
+            if (!vis[neighbor]) {
+                if (dfs(neighbor, adj, vis, pathVis) == true) {
+                    return true;
+                }
+            else if (pathVis[node]) return true;
+            }
+        }
+
+        pathVis[node] = false;
+        return false;
+    }
+
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] vis = new boolean[V];
+        boolean[] pathVis = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                if (dfs(i, adj, vis, pathVis) == true) return true;
+            }
+        }
+        
+        return false;
+    }
+}
