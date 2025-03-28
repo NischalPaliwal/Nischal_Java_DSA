6dd2e30afec1d0b0b7751f380eae73535f11fcff
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-03-28 17:54:48 +0000

    code added

---
+++ graphs/topoSort.java
@@ -0,0 +1,39 @@
+package graphs;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class topoSort {
+    public static void main(String[] args) {
+        
+    }
+
+    private static void dfs(int node, boolean[] vis, Stack<Integer> st, ArrayList<ArrayList<Integer>> adj) {
+        vis[node] = true;
+        for (int neighbor : adj.get(node)) {
+            if (!vis[neighbor]) {
+                dfs(neighbor, vis, st, adj);
+            }
+        }
+        st.push(node);
+    }
+
+    public static int[] sort(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] vis = new boolean[V];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfs(i, vis, st, adj);
+            }
+        }
+
+        int[] ans = new int[V];
+        int i = 0;
+        while (!st.isEmpty()) {
+            ans[i++] = st.peek();
+            st.pop();
+        }
+
+        return ans;
+    }
+}
